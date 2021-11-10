1c81d572f1ef70c7e81174885ef17c64c4a54a65
ASendellPrice <36015770+ASendellPrice@users.noreply.github.com>
2021-11-10 14:28:56 +0100

    Update Calculate_FST_from_SFS_ANGSD.sh

--- Calculate_FST_from_SFS_ANGSD.sh
+++ Calculate_FST_from_SFS_ANGSD.sh
@@ -90,10 +90,10 @@ then
     angsd -bam $POP2_BAM_LIST -ref $REF -anc $ANC -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 0 -trim 0 -minMapQ 20 -minQ 20 -gl 1 -doSaf 1 -r $CHROM -out pop2.${CHROM}
 
     #Calculate the 2D site frequency spectrum
-    #-fold 1: 			tells realSFS to output the "folded" site frequency spectrum
-    #-m 0: tells		tells realSFS to use the standard EM algorithm instead of the accelerated EM algorithm
-    #-maxIter 50000: 	sets the maximum number of EM iterations to 50,000 instead of the default 100 (to provide sufficient iterations for covergence)
-    #-tole 1e-6: 		when the difference in successive likelihood values in the EM algorithm gets below this value the optimisation will stop.
+    #-fold 1:   tells realSFS to output the "folded" site frequency spectrum
+    #-m 0:  tells realSFS to use the standard EM algorithm instead of the accelerated EM algorithm
+    #-maxIter 50000:    sets the maximum number of EM iterations to 50,000 instead of the default 100 (to provide sufficient iterations for covergence)
+    #-tole 1e-6:    when the difference in successive likelihood values in the EM algorithm gets below this value the optimisation will stop.
     realSFS pop1.${CHROM}.saf.idx pop2.${CHROM}.saf.idx -fold 1 -m 0 -maxIter 50000 -tole 1e-4 > pop1.pop2.${CHROM}.folded.sfs
 
     #Calculate FST on a per site basis
@@ -121,10 +121,10 @@ then
     angsd -bam $POP2_BAM_LIST -ref $REF -anc $ANC -uniqueOnly 1 -remove_bads 1 -only_proper_pairs 0 -trim 0 -minMapQ 20 -minQ 20 -gl 1 -doSaf 1 -r $CHROM -out pop2.${CHROM}
 
     #Calculate the 2D site frequency spectrum
-    #-fold 0: 			tells realSFS to output the "unfolded" site frequency spectrum
-    #-m 0: tells		tells realSFS to use the standard EM algorithm instead of the accelerated EM algorithm
-    #-maxIter 50000: 	sets the maximum number of EM iterations to 50,000 instead of the default 100 (to provide sufficient iterations for covergence)
-    #-tole 1e-6: 		when the difference in successive likelihood values in the EM algorithm gets below this value the optimisation will stop.
+    #-fold 0:   tells realSFS to output the "unfolded" site frequency spectrum
+    #-m 0:  tells realSFS to use the standard EM algorithm instead of the accelerated EM algorithm
+    #-maxIter 50000:    sets the maximum number of EM iterations to 50,000 instead of the default 100 (to provide sufficient iterations for covergence)
+    #-tole 1e-6:    when the difference in successive likelihood values in the EM algorithm gets below this value the optimisation will stop.
     realSFS pop1.${CHROM}.saf.idx pop2.${CHROM}.saf.idx -fold 0 -m 0 -maxIter 50000 -tole 1e-4 > pop1.pop2.${CHROM}.unfolded.sfs
 
     #Calculate FST on a per site basis
